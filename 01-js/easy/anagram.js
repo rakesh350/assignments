fd9efa7bc84c3dc2f3ecb0f830ef83c04ee7b85a
Rakesh Mishra <jrakesh173@gmail.com>
2023-12-09 23:24:36 -0800

    - Anagram passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+    // create two array of strings 
+    const str1Arr = str1.toUpperCase().split('');
+    const str2Arr = str2.toUpperCase().split('');
 
+    // Loop through one string and pop the element, if loop continues till end of loop, its anagram else not
+    for(let i = 0; i < str1Arr.length; i++) {
+      if(str2Arr.indexOf(str1Arr[i]) === -1) {
+        return false
+      } else {
+        str2Arr.splice(str2Arr.indexOf(str1Arr[i]), 1)
+      }
+    }
+    if(str2Arr.length === 0) {
+      return true;
+    }
+    return false;
 }
-
 module.exports = isAnagram;

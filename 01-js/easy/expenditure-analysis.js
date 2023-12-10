8b62e2752c7ee8acf1966fb68b22bed52c38fadb
Rakesh Mishra <jrakesh173@gmail.com>
2023-12-10 00:51:36 -0800

    Removed unused code

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -28,7 +28,6 @@ function calculateTotalSpentByCategory(transactions) {
   return totalSpentByCategory;
 }
 
-
 function isCategoryExists(spendingList, category) {    
     for (let i = 0; i< spendingList.length; i++) {      
       if(spendingList[i].category === category) {
@@ -38,43 +37,4 @@ function isCategoryExists(spendingList, category) {
     return -1;
 }
 
-const transactions = [
-  {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: 'Food',
-    itemName: 'Pizza',
-  },
-  {
-    id: 2,
-    timestamp: 1656259600000,
-    price: 20,
-    category: 'Food',
-    itemName: 'Burger',
-  },
-  {
-    id: 3,
-    timestamp: 1656019200000,
-    price: 15,
-    category: 'Clothing',
-    itemName: 'T-Shirt',
-  },
-  {
-    id: 4,
-    timestamp: 1656364800000,
-    price: 30,
-    category: 'Electronics',
-    itemName: 'Headphones',
-  },
-  {
-    id: 5,
-    timestamp: 1656105600000,
-    price: 25,
-    category: 'Clothing',
-    itemName: 'Jeans',
-  },
-];
-
-calculateTotalSpentByCategory(transactions);
 module.exports = calculateTotalSpentByCategory;

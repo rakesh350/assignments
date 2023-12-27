ad1cd1007a7c721ae2778cd46fc7649fd8a142ff
Rakesh Mishra <jrakesh173@gmail.com>
2023-12-27 10:55:34 -0800

    01-JS completed

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,52 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (this.todos[indexOfTodo] !== undefined) {
+      this.todos.splice(indexOfTodo, 1);
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (this.todos[index] !== undefined) {
+      this.todos[index] = updatedTodo;
+    }
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (this.todos[indexOfTodo] !== undefined) {
+      return this.todos[indexOfTodo];
+    } 
+    return null;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
+
+// let todayTodo = new Todo();
+// todayTodo.add("SES Document Components 50%");
+// todayTodo.add("Spatial Index V2 20%");
+// todayTodo.add("Send data from EAM to ODS");
+// todayTodo.add("Send data from ISU to ODS");
+// todayTodo.add("Review the two pending items for Schema comparison");
+// console.log(todayTodo.getAll());
+// todayTodo.remove(4);
+// console.log(todayTodo.getAll());
+// todayTodo.update(1, "Spatial Index V2 30%");
+// console.log(todayTodo.getAll());
 
 module.exports = Todo;
